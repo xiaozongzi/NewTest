093e62876f9d1887107477227b2b1d26cd569dde
zhang zhongzhi <478359031@qq.com>
2016-04-07 14:22:22 +0800

    Signed-off-by: zhang zhongzhi <478359031@qq.com>

---
+++ app/src/main/java/com/io/vov/vitamio/newtest/app/PlaceholderFragment.java
@@ -0,0 +1,56 @@
+package com.io.vov.vitamio.newtest.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+/**
+ * Created by ${zhangzz} on 2016/4/7.
+ */
+public class PlaceholderFragment  extends Fragment {
+    /**
+     * The fragment argument representing the section number for this
+     * fragment.
+     */
+    private ListView listView;
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static PlaceholderFragment newInstance(int sectionNumber) {
+        PlaceholderFragment fragment = new PlaceholderFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public PlaceholderFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        init(rootView);
+        return rootView;
+    }
+
+    private void init(View view) {
+        listView= (ListView) view.findViewById(R.id.list_view);
+
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        ((MainActivity) activity).onSectionAttached(
+                getArguments().getInt(ARG_SECTION_NUMBER));
+    }
+}
