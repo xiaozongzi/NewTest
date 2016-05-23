575808ab6d603295545b228e83dd4bf773480b6e
xiaozongzi <478359031@qq.com>
2016-05-23 17:34:15 +0800

    160523 画×

--- Libray/src/main/java/com/io/vov/vitamio/libray/lib/BaseActivity.java
+++ Libray/src/main/java/com/io/vov/vitamio/libray/lib/BaseActivity.java
@@ -1,11 +1,12 @@
 package com.io.vov.vitamio.libray.lib;
 
 import android.app.ActivityManager;
-import android.content.ComponentName;
-import android.content.Context;
+import android.app.KeyguardManager;
+import android.content.*;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.widget.Toast;
 
 import java.util.List;
@@ -15,6 +16,7 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        startScreenBroadcastReceiver();
 
     }
     public void showToast(String msg){
@@ -24,13 +26,65 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onStop() {
         super.onStop();
-        if (isAppOnForeground()){
+      /*  if (isAppOnForeground()){
             showToast("app foreground");
+            Log.e("onStop","app  foreground");
         }else {
             showToast("app not foreground");
-        }
+            Log.e("onStop","app not foreground");
+        }*/
     }
 
+    /**
+     * 为true，表示有两种状态：a、屏幕是黑的 b、目前正处于解锁状态 。
+     为false，表示目前未锁屏
+        还可以通过广播来实现 {@value mScreenReceiver}
+     * @return
+     */
+    KeyguardManager mKeyguardManager;
+    private boolean isScreenOff(){
+        if (mKeyguardManager==null)
+         mKeyguardManager = (KeyguardManager)getSystemService(Context.KEYGUARD_SERVICE);
+        return  mKeyguardManager.inKeyguardRestrictedInputMode();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (event.getAction()==event.KEYCODE_HOME){
+            Log.e("onStop","home press"); //home 无法监听
+        }
+        return super.onKeyDown(keyCode, event);
+
+    }
+
+    private ScreenBroadcastReceiver mScreenReceiver;
+    private class ScreenBroadcastReceiver extends BroadcastReceiver {
+        private String action = null;
+
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            action = intent.getAction();
+            if (Intent.ACTION_SCREEN_ON.equals(action)) {
+                // 开屏
+                Log.e("onStop"," 开屏");
+            } else if (Intent.ACTION_SCREEN_OFF.equals(action)) {
+                // 锁屏
+                Log.e("onStop","锁屏");
+            } else if (Intent.ACTION_USER_PRESENT.equals(action)) {
+                // 解锁
+                Log.e("onStop","解锁 ");
+            }
+        }
+    }
+    private void startScreenBroadcastReceiver() {
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_SCREEN_ON);
+        filter.addAction(Intent.ACTION_SCREEN_OFF);
+        filter.addAction(Intent.ACTION_USER_PRESENT);
+        mScreenReceiver=new ScreenBroadcastReceiver();
+        registerReceiver(mScreenReceiver, filter);
+    }
     /**
      * 判断app是否 正在显示 推荐使用这种方式
      * @return
